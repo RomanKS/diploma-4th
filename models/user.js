ccc5e8dde9b241d4d62e3138a207f3d52db0a7a4
Roman <roma.rec.cm@gmail.com>
2020-04-12 17:20:15 +0300

    2 : adding of basic passport local auntification was added basic passport local auntification

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = (sequelize, type) => {
     const User = sequelize.define('User', {
         ID: {
@@ -16,6 +18,29 @@ module.exports = (sequelize, type) => {
     User.associate = function (modules) {
         User.belongsTo(modules.UserType, {foreignKey: 'FK_UserType', as: 'UserType'});
     };
+
+    User.prototype.validatePassword = function (password) {
+        return this.Password == password;
+    };
+    User.prototype.generateJWT = function() {
+        const today = new Date();
+        const expirationDate = new Date(today);
+        expirationDate.setDate(today.getDate() + 60);
+
+        return jwt.sign({
+            email: this.UserName,
+            id: this.ID,
+            exp: parseInt(expirationDate.getTime() / 1000, 10),
+        }, 'secret');
+    };
+    User.prototype.toAuthJSON = function() {
+        return {
+            _id: this.ID,
+            email: this.UserName,
+            token: this.generateJWT(),
+        };
+    };
+
 
     return User;
 };
